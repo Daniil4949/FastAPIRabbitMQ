ab98d79807a6005cf2cef0c7bc9de0bd50e0236f
daniil <daniilkimstachp@gmail.com>
2023-02-16 18:16:29 +0300

    Mongo was fixed

--- app/mongodb.py
+++ app/mongodb.py
@@ -3,7 +3,7 @@ from pymongo.server_api import ServerApi
 import pymongo
 
 # Replace <connection string> with your MongoDB deployment's connection string.
-conn_str = "mongodb://root:password@172.20.0.2:27017"
+conn_str = "mongodb://root:password@mongo_db:27017"
 # Set the Stable API version on the client.
 client = pymongo.MongoClient(conn_str, server_api=ServerApi('1'), serverSelectionTimeoutMS=5000)
 
@@ -14,8 +14,7 @@ users = mydb["users"]
 def insert_data(data: str):
     user_id: str = data.split()[0]
     user_info: str = " ".join(data.split()[1:])
-    print(user_info)
-    result = users.insert_one({user_id: user_info})
+    result = users.insert_one({"id": user_id, "user_info": user_info})
     return result
 
 
@@ -26,7 +25,7 @@ def get_data(id: int):
     return result
 
 
-# get_data(6)
+print(get_data(6))
 
 
 
